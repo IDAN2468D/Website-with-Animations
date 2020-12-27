920d716d82ed7c8d0e1530406e615b2b52415a59
IDAN <idankzm@gmai.com>
2020-12-28 01:10:28 +0200

    Hero+APP

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './gloobalStyle';
 import Hero from './components/Hero';
+import { SliderData } from './data/SliderData';
 
 
 function App() {
